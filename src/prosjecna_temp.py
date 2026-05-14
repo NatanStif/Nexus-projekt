421da3c07a553f7d6d3f386c019da4f47ecbd3b1
NatanStif <natan.stifanic@gmail.com>
2026-05-14 09:48:25 +0200

    Update prosjecna_temp.py

--- src/prosjecna_temp.py
+++ src/prosjecna_temp.py
@@ -1,12 +1,9 @@
-import matplotlib.pyplot as plt
-import seaborn as sns
 import pandas as pd
-import json
-import requests
 
 
 df_lokacije = pd.read_csv("data/mars_lokacije.csv", sep = ";", decimal = ",")
 df_uzorci = pd.read_csv("data/mars_uzorci.csv", sep = ";", decimal = ",")
+
 
 df_spojeno = pd.merge(
     df_lokacije,
@@ -15,11 +12,7 @@ df_spojeno = pd.merge(
 
 
 df_filtrirano = df_spojeno[df_spojeno["Temp_Tla_C"] < 150]
-ukupna_temp = 0
+prosjek = df_filtrirano["Temp_Tla_C"].mean()
 
 
-for i in range(0,len(df_filtrirano)):
-    ukupna_temp += df_filtrirano["Temp_Tla_C"][i]
-
-
-print(ukupna_temp/len(df_filtrirano))
+print(f"Prosjek = {round(prosjek,2)}°C")
