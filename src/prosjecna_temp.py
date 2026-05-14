cedce589a5b3e6064587c99bd11fcfd6129ab518
NatanStif <natan.stifanic@gmail.com>
2026-05-14 09:19:54 +0200

    Update prosjecna_temp.py

--- src/prosjecna_temp.py
+++ src/prosjecna_temp.py
@@ -4,5 +4,19 @@ import pandas as pd
 import json
 import requests
 
+
 df_lokacije = pd.read_csv("data/mars_lokacije.csv", sep = ";", decimal = ",")
 df_uzorci = pd.read_csv("data/mars_uzorci.csv", sep = ";", decimal = ",")
+
+df_spojeno = pd.merge(
+    df_lokacije,
+    df_uzorci,
+    on="ID_Uzorka")
+
+
+df_filtrirano = df_spojeno[df_spojeno["Temp_Tla_C"] < 150]
+ukupna_temp = 0
+
+
+for i in range(0,len(df_filtrirano)):
+  ukupna_temp = 
