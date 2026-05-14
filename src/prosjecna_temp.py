39ea2ae82b42013038eff02c4d9c54e21fad3fbb
NatanStif <natan.stifanic@gmail.com>
2026-05-14 09:18:05 +0200

    Update prosjecna_temp.py

--- src/prosjecna_temp.py
+++ src/prosjecna_temp.py
@@ -1,4 +1,8 @@
-
+import matplotlib.pyplot as plt
+import seaborn as sns
+import pandas as pd
+import json
+import requests
 
 df_lokacije = pd.read_csv("data/mars_lokacije.csv", sep = ";", decimal = ",")
 df_uzorci = pd.read_csv("data/mars_uzorci.csv", sep = ";", decimal = ",")
